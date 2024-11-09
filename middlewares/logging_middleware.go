5a790699b3820241b405a457d9d2d3bcf923ba44
Randy Steven <randysteven12@gmail.com>
2024-11-09 13:18:09 +0700

    logging middleware fixing

--- middlewares/logging_middleware.go
+++ middlewares/logging_middleware.go
@@ -11,25 +11,30 @@ import (
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Entering logging middleware")
-
+		var (
+			body []byte = nil
+			err  error  = nil
+		)
 		requestTime := time.Now()
 
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Failed to read request body: %v\n", err)
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
+		if r.Method != http.MethodGet {
+			body, err = ioutil.ReadAll(r.Body)
+			if err != nil {
+				log.Printf("Failed to read request body: %v\n", err)
+				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
+			r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		}
-
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
 		next.ServeHTTP(w, r)
 
 		var requestBody interface{}
-		if err := json.Unmarshal(body, &requestBody); err != nil {
-			log.Printf("Failed to unmarshal request body: %v\n", err)
-			return
+		if body != nil {
+			if err := json.Unmarshal(body, &requestBody); err != nil {
+				log.Printf("Failed to unmarshal request body: %v\n", err)
+				return
+			}
 		}
 
 		log.Printf("%s %s %s %s\n", requestTime.Format("2006-01-02 15:04:05"), r.Method, r.URL.Path, requestBody)
