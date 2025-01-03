ccef329723333876c4aa7a1535b7c6cb60c48ec1
Randy Steven <randysteven12@gmail.com>
2025-01-03 18:32:56 +0700

    log handling

--- middlewares/logging_middleware.go
+++ middlewares/logging_middleware.go
@@ -2,18 +2,38 @@ package middlewares
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"github.com/RandySteven/Library-GO/enums"
+	"github.com/google/uuid"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 )
 
+type ResponseWriterWrapper struct {
+	http.ResponseWriter
+	Body       *bytes.Buffer
+	StatusCode int
+}
+
+func (rw *ResponseWriterWrapper) WriteHeader(statusCode int) {
+	rw.StatusCode = statusCode
+	rw.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (rw *ResponseWriterWrapper) Write(b []byte) (int, error) {
+	rw.Body.Write(b)
+	return rw.ResponseWriter.Write(b)
+}
+
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var (
 			body []byte = nil
 			err  error  = nil
+			ctx         = context.WithValue(r.Context(), enums.RequestID, uuid.NewString())
 		)
 		requestTime := time.Now()
 
@@ -27,7 +47,14 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 			r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 		}
 
-		next.ServeHTTP(w, r)
+		rw := &ResponseWriterWrapper{
+			ResponseWriter: w,
+			Body:           &bytes.Buffer{},
+			StatusCode:     http.StatusOK,
+		}
+
+		r.WithContext(ctx)
+		next.ServeHTTP(rw, r)
 
 		var requestBody interface{}
 		if body != nil {
@@ -37,6 +64,30 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 			}
 		}
 
-		log.Printf("%s %s %s %s\n", requestTime.Format("2006-01-02 15:04:05"), r.Method, r.URL.Path, requestBody)
+		var responseBody interface{}
+		if rw.Body.Len() > 0 {
+			if err := json.Unmarshal(rw.Body.Bytes(), &responseBody); err != nil {
+				log.Printf("Failed to unmarshal response body: %v\n", err)
+			}
+		}
+
+		logEntry := map[string]interface{}{
+			"Request ID ": ctx.Value(enums.RequestID).(string),
+			"timestamp":   requestTime.Format("2006-01-02 15:04:05"),
+			"method":      r.Method,
+			"path":        r.URL.Path,
+			"remote":      r.RemoteAddr,
+			"request":     requestBody,
+			"response":    responseBody,
+			"status":      rw.StatusCode,
+		}
+
+		logEntryJSON, err := json.Marshal(logEntry)
+		if err != nil {
+			log.Printf("Failed to marshal log entry: %v\n", err)
+			return
+		}
+
+		log.Println(string(logEntryJSON))
 	})
 }
